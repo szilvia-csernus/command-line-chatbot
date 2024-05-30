5f37cb68aa414915e76d772ee0ecf166818af6a6
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2024-05-30 18:56:03 +0100

    add basic chatbot functionality

--- chatbot.py
+++ chatbot.py
@@ -1,1 +1,25 @@
 import openai
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+openai.api_key = config["OPENAI_API_KEY"]
+
+messages = []
+while True:
+  try:
+    user_input = input("You: ")
+    messages.append({
+      "role": "user",
+      "content": user_input
+    })
+    response = openai.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+        max_tokens=50
+    )
+    print("AI: ", response.choices[0].message.content)
+
+  except KeyboardInterrupt:
+    print("Exiting...")
+    break
+
